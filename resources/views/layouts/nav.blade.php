35ab2e116fab6fbd9ae235135f67585c0a9f6349
Deepak <structllooper@gmail.com>
2019-10-23 11:38:57 +0530

    stable 22

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -32,13 +32,13 @@ body{
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
 <header class="header_area ">
-        <div class="main_menu  border border-dark pt-4 rounded" style="background-color:pink;  " >
+        <div class="main_menu  border border-top-0 border-dark ml-1  rounded" style="background-color:pink;  " >
             <div class="container-fluid  ">
                 <nav class="navbar navbar-expand-lg   navbar-light">
                     <div class="container">
                         <!-- Brand and toggle get grouped for better mobile display -->
-                        <a class="navbar-brand logo_h" href="/"><img src="/icons/lgo.png" alt="not found" width="30px" height="30px"><div class="text-dark">Charity</div></a>
-                        <a href="/donate" type="button" class="btn btn-danger ml-3">Donate Us!!</a>
+                        <a class="navbar-brand logo_h" href="/"><img src="/icons/lgo.png" alt="not found" width="50px" height="50px"><div class="text-dark">Charity</div></a>
+                        <a href="/donate" type="button" class="btn btn-success ml-3">Donate Us!!</a>
                         <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="icon-bar text-dark">menu</span>
                             <span class="icon-bar"></span>
@@ -128,5 +128,5 @@ body{
 
    
   
-
+<hr>
   @yield('nav_str')
