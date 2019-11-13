86c246e4a76eec11ca1307496cd8b520332ed8b3
Deepak <structllooper@gmail.com>
2019-11-13 11:55:44 +0530

    data_base_table_created

---
+++ database/migrations/2019_11_13_061543_create_home_health_slide.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHomeHealthSlide extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('home_health_slide', function (Blueprint $table) {
+            $table->string('heading');
+            $table->string('content',2000);
+            $table->mediumText('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('home_health_slide');
+    }
+}
