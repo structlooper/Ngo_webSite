175d193226cf3b829fd4964f2077ab82034fbf25
Deepak <structllooper@gmail.com>
2019-11-18 11:14:29 +0530

    routeConfiguring

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,13 @@
 
 
 Route::get('/', 'IndexController@index')->name('index');
-Route::get('/donate','IndexController@donate');
-Route::get('/contactUs','IndexController@contact')->name('contact_us');
+Route::get('/donate','IndexController@donate')->name('dontePage');
+Route::get('/contactUs','IndexController@contact')->name('contactUs');
 
 // about_us......
-Route::get('/aboutus/who_we_are','AboutController@about_us');
+Route::get('/aboutus/who_we_are','AboutController@about_us')->name('who_we_are');
 Route::get('/aboutus','AboutController@about_us');
-Route::get('/aboutus/team','AboutController@team');
+Route::get('/aboutus/team','AboutController@team')->name('team');
 
 
 //  Events.......
@@ -28,60 +28,60 @@ Route::get('/events','EventsController@index')->name('events');
 
 // our works.......
 
-Route::get('/our_works/education','OurWorkController@education');
-Route::get('/our_works/health','OurWorkController@health');
-Route::get('/our_works/WomenEnpowerment','OurWorkController@women');
+Route::get('/our_works/education','OurWorkController@education')->name('education');
+Route::get('/our_works/health','OurWorkController@health')->name('health');
+Route::get('/our_works/WomenEnpowerment','OurWorkController@women')->name('WomenEnpowerment');
 
 Auth::routes();
 
 
 Route::post('/donating', 'DonateController@store')->name('donating');
-Route::post('/paytm-callback','DonateController@paytmCallback');
+Route::post('/paytm-callback','DonateController@paytmCallback')->name('paytm-callback');
 
-Route::post('/sendMail', 'MailController@send')->name('SendMail');
+Route::post('/sendMail', 'MailController@send')->name('SendMail')->name('sendMail');
 
 Route::group(['middleware' => ['auth','Admin']], function () {
     
 
     Route::any('/dashboard', 'DashboardController@index')->name("dashboard");
-    Route::get('/profiles','DashboardController@profiles');
+    Route::get('/profiles','DashboardController@profiles')->name("profiles");
     
 
-    Route::get('/aboutusEdit','AboutEditWhoWeAreController@index');
+    Route::get('/aboutusEdit','AboutEditWhoWeAreController@index')->name("aboutusEdit");
     Route::post('/aboutSave','AboutEditWhoWeAreController@store')->name('aboutSave');
     
 
-    Route::get('/aboutusEditTeam','AboutTeamEditController@index_1');
+    Route::get('/aboutusEditTeam','AboutTeamEditController@index_1')->name("aboutusEditTeam");
     Route::post('/slideData','AboutTeamEditController@store_slide_data')->name('SaveData');
     Route::post('/departmentMember','AboutTeamEditController@add_member_department')->name('SaveDeaprtmentMember');
 
 
-    Route::get('/ourWorksEducationEdit','OurWorkEditController@Education');
+    Route::get('/ourWorksEducationEdit','OurWorkEditController@Education')->name("ourWorksEducationEdit");
     Route::post('/educationSlideSave','OurWorkEditController@educationSlideSave')->name('educationSlideSave');
     Route::post('/educationSideDataSave','OurWorkEditController@educationSideDataSave')->name('educationSideDataSave');
     Route::post('/educationSpecificWorkSave','OurWorkEditController@educationSpecificWorkSave')->name('educationSpecificWorkSave');
     
-    Route::get('/ourWorksHealthEdit','OurWorkEditController@Health');
+    Route::get('/ourWorksHealthEdit','OurWorkEditController@Health')->name("ourWorksHealthEdit");
     Route::post('/healthSlideSave','OurWorkEditController@healthSlideSave')->name('healthSlideSave');
     Route::post('/healthSideDataSave','OurWorkEditController@healthSideDataSave')->name('healthSideDataSave');
     Route::post('/healthSpecificWorkSave','OurWorkEditController@healthSpecificWorkSave')->name('healthSpecificWorkSave');
     
     
-    Route::get('/ourWorksWomenEnpowermentEdit','OurWorkEditController@womenEnpowerment');
+    Route::get('/ourWorksWomenEnpowermentEdit','OurWorkEditController@womenEnpowerment')->name("ourWorksWomenEnpowermentEdit");
     Route::post('/womenSlideSave','OurWorkEditController@womenSlideSave')->name('womenSlideSave');
     Route::post('/womenSideDataSave','OurWorkEditController@womenSideDataSave')->name('womenSideDataSave');
     Route::post('/womenSpecificWorkSave','OurWorkEditController@womenSpecificWorkSave')->name('womenSpecificWorkSave');
     
-    Route::get('/eventsEdit' , 'EventsEditController@events');
+    Route::get('/eventsEdit' , 'EventsEditController@events')->name("eventsEdit");
     Route::post('/eventSlideSave' , 'EventsEditController@eventSlideDataSave')->name('eventSlideDataSave');
     Route::post('/eventUpcomingSave' , 'EventsEditController@eventUpcomingDataSave')->name('eventUpcomingDataSave');
     Route::post('/eventHistorySave' , 'EventsEditController@eventHistoryDataSave')->name('eventHistoryDataSave');
     
-    Route::get('/donateEdit','DonateEditController@donateEdit');
+    Route::get('/donateEdit','DonateEditController@donateEdit')->name("donateEdit");
     Route::post('/donateDataSave' , 'DonateEditController@donateDataSave')->name('donatePageDataSave');
     
 
-    Route::get('/homepageEdit' ,'HomePageEditController@view');
+    Route::get('/homepageEdit' ,'HomePageEditController@view')->name("homepageEdit");
     Route::post('/homeSlideSave','HomePageEditController@homeSlideSave')->name('homeSlideSave');
     Route::post('/homeAboutNgoDataSave','HomePageEditController@homeAboutNgoDataSave')->name('homeAboutNgoDataSave');
     Route::post('/homeEducationSlideSave','HomePageEditController@homeEducationSlideSave')->name('homeEducationSlideSave');
@@ -89,7 +89,7 @@ Route::group(['middleware' => ['auth','Admin']], function () {
     Route::post('/homeCretingImpactSave','HomePageEditController@homeCretingImpactSave')->name('homeCretingImpactSave');
     
     
-    Route::get('/contactPageEdit', 'ContactPageEditController@contact');
+    Route::get('/contactPageEdit', 'ContactPageEditController@contact')->name("contactPageEdit");
     Route::post('/contactDetailsSave','ContactPageEditController@contactDetailsSave')->name('contactDetailsSave');
 
 });
